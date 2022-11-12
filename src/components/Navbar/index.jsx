fdbfa8398d297837a788d28905a98a983419048d
Samandar0021305 <tsamandardev89@gmail.com>
2022-11-12 17:57:08 +0500

    init

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
-import {Container, Wrapper} from './style';
+import {Container, Logo, Section, Wrapper} from './style';
+import logoImg from "../../assets/icons/logo.svg"
 
 const Home = () => {
   return (
     <Container>
       <Wrapper>
-        <h1>Test</h1>
+        <Section>
+          <Logo src={logoImg} />
+        </Section>
+        <Section>2</Section>
+        <Section>3</Section>
       </Wrapper>
       <Outlet />
     </Container>
--- src/components/Navbar/style.js
+++ src/components/Navbar/style.js
@@ -1,18 +1,26 @@
 import  styled  from "styled-components"
 
-
 const Container = styled.div`
 `
 const Wrapper = styled.div`
 display:flex;
 align-items:center;
+justify-content:space-between;
 background:var(--colorPrimary);
 color:#fff;
 padding:var(--padding);
 height:64px;
+font-size:16px;
 `
+const Section = styled.div`
+` 
+const Logo = styled.img`
+width:30px;
+` 
 
 export {
     Container,
-    Wrapper
+    Wrapper,
+    Section,
+    Logo
 }
