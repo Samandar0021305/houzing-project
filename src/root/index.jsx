e21b6a912e61a398a1bb3836711418e9e30a943d
Samandar0021305 <tsamandardev89@gmail.com>
2022-11-12 13:51:13 +0500

    init

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Routes,Route, Navigate } from 'react-router-dom'
 
  const Root = () => {
   return (
-    <div>Root</div>
+    <BrowserRouter>
+     <Routes>
+       <Route path='/home' element={<h1>Home</h1>}/>
+       <Route  path='/properties'  element={<h2>Properties</h2>} />
+       <Route path='/' element={<Navigate to={'/home'} />} />
+       <Route path='*' element={<h1>404 NOR FOUND</h1>}  />
+     </Routes>
+    </BrowserRouter>
   )
 }
 
